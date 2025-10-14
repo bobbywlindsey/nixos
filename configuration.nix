d276e955290ddce2f66ecd49884b6cc30a0029bc
Bobby Lindsey <contact@bobbywlindsey.com>
2025-10-13 23:56:10 -0600

    Use ly for login manager

--- configuration.nix
+++ configuration.nix
@@ -49,8 +49,11 @@
   services.xserver.enable = true;
 
   # Enable the KDE Plasma Desktop Environment
-  services.displayManager.sddm.enable = true;
+  #services.displayManager.sddm.enable = true;
   services.desktopManager.plasma6.enable = true;
+
+  # Display/login manager for Wayland
+  services.displayManager.ly.enable = true;
 
   # Configure keymap in X11
   services.xserver.xkb = {
